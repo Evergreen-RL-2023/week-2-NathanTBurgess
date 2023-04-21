fc9ee05b95b4950c729e37f420c54563453b3ff4
Nathan Burgess <Nathantburgess@gmail.com>
2023-04-21 11:47:49 -0400

    Finished lab

--- rl_k_bandits.py
+++ rl_k_bandits.py
@@ -7,9 +7,10 @@ I used imperative here
 import numpy as np
 import random as rnd
 
+e = .01
 k = 10  # number of bandits
 q_star = np.zeros([k])
-rewards = np.zeros([k])
+rewards = [rnd.uniform(0,100) for _ in range(k)]
 counts = np.zeros([k])
 
 
@@ -23,13 +24,17 @@ def init_bandits():
 
 def run_bandits(n_steps):
     for i in range(n_steps):
-        counts[0] += 1
+        for i in range(k):
+            a = rnd.choices([rnd.choice(np.flatnonzero(q_star == q_star.max())), rnd.randint(0,k-1)], weights=((1-e),e))[0]
+            print(a)
+            counts[a] += 1
+            q_star[a] = q_star[a] + (1/counts[a]) * (rewards[a] - q_star[a])
 
 
 if __name__ == '__main__':
-    rnd.seed(42)
+    rnd.seed(rnd.randint(0,1000))
     init_bandits()
     x = run_bandits(1000)
 
     for i in range(k):
-        print(q_star[i])
+        print("estimate: ", q_star[i], "     true value: ", rewards[i])
